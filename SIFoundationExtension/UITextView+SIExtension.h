613761095dfa475e5d84a41d23d69a935262ed99
Matias Pequeno <matias.pequeno@gmail.com>
2012-09-27 20:14:21 -0300

    Added UITextView extension.

---
+++ SIFoundationExtension/UITextView+SIExtension.h
@@ -0,0 +1,13 @@
+//
+//  UITextView+SIExtension.h
+//  weheartit
+//
+//  Created by Matias Pequeno on 9/27/12.
+//  Copyright (c) 2012 WHI Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextView (SIExtension)
+
+@end
---
+++ SIFoundationExtension/UITextView+SIExtension.m
@@ -0,0 +1,13 @@
+//
+//  UITextView+SIExtension.m
+//  weheartit
+//
+//  Created by Matias Pequeno on 9/27/12.
+//  Copyright (c) 2012 WHI Inc. All rights reserved.
+//
+
+#import "UITextView+SIExtension.h"
+
+@implementation UITextView (SIExtension)
+
+@end
